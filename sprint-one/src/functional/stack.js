910c84062f0d9ec999497a867d46c1a2212c763a
Eric Fischer <ericf712@gmail.com>
2015-07-01 14:42:15 -0700

    Functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,33 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    length++;
+    storage[length] = value;
   };
 
   someInstance.pop = function(){
+    if (length <= 0) {
+      return 0;
+    }
+    length--;
+    return storage[length+1];
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
 };
+
+
+var stackOfDishes = Stack();
+stackOfDishes.size();
+stackOfDishes.push(platter);
+stackOfDishes.push(plate);
+stackOfDishes.pop();
+
