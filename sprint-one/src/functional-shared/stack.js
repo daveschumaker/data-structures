187166094e505a00a6521fe2823840a0a90d5b44
Hack Reactor Students <accounts@hackreactor.com>
2015-07-01 21:12:38 -0700

    "Makes progress on stacks and queues"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+  	length: 0,
+  	storage: {}
+  };
+  extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this.length++;
+		this.storage[this.length] = value;
+	},
+	size: function() {
+		return this.length;
+	},
+	pop: function() {
+		if (this.length <= 0) {
+			return 0;
+		}
+		this.length--;
+		return this.storage[this.length+1];
+	}
+};
 
 
+var extend = function(to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
+}
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,32 +8,21 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-  	length++;
-  	storage[length] = value;
+    length++;
+    storage[length] = value;
   };
 
   someInstance.dequeue = function(){
-  	var result;
-  	start++;
-
-  	if (length-start <= 0) {
-  		return 0;
-  	}
-    
-    result = storage[start-1];
-    // storage[start] = undefined;
-    //delete storate[start];
-    //storage[length] = undefined;
     //length--;
-    return result;
+    start++;
+    return storage[start];
   };
 
   someInstance.size = function(){
-
-  	if (length-start <= 0) {
-  		return 0;
-  	}
-  	return length-start;
+    if (length-start <= 0) {
+      return 0;
+    }
+    return length-start;
   };
 
   return someInstance;
@@ -42,9 +31,9 @@ var Queue = function(){
 /*
 queue.enqueue('a'); // Resulting object: {1: "a"}
 queue.enqueue('b');	// {1: "a", 2: "b"}
-queue.dequeue();	// DEQUEUED: 'a' -> {2: "b"}
+queue.dequeue();	// DEQUEUED: 'a'   // Left with -> {2: "b"}
 queue.enqueue('c');	// QUEUED: 'c' -> {2: "b", 3: "c"}
-expect(queue.dequeue()).to.equal('b');  // {2: "b"}
+expect(queue.dequeue()).to.equal('b');  // DEQUEDED return: 'b'   // Left with -> {3: "c"}
 */
 
 /*
@@ -53,4 +42,63 @@ queue.enqueue('b');
 queue.dequeue();
 queue.enqueue('c');
 expect(queue.dequeue()).to.equal('b');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+  var length = 0;
+  var start = 0;
+  // Implement the methods below
+
+  someInstance.enqueue = function(value){
+    length++;
+    storage[length] = value;
+  };
+
+  someInstance.dequeue = function(){
+    start++;
+    return storage[start];
+  };
+
+  someInstance.size = function(){
+    if (length-start <= 0) {
+      return 0;
+    }
+    return length-start;
+  };
+
+  return someInstance;
+
 */
