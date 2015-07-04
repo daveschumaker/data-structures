37893bd4da544c2971945bea4fbb44a87a4c14dc
Hack Reactor Students <accounts@hackreactor.com>
2015-07-04 10:27:06 -0700

    First 3 tests for Hash Tables are now passing.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,12 +4,19 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  // 1. Get index (address) for our key (k)
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // 2. Once we have address, insert into our table at 'i' (address!)
+  this._storage[i] = [k,v];
 };
 
 HashTable.prototype.retrieve = function(k){
+  // 1. Get index (address) for our key (k)
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  // 2. Return value associated with k.
+  return this._storage[i][1];
 };
 
 HashTable.prototype.remove = function(k){
@@ -21,3 +28,7 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ var myTable = new HashTable();
+ myTable.insert('Eric','Fischer');
+
