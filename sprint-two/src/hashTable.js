bec77f36a762a7c312eadeb7612d071d126c0df6
Hack Reactor Students <accounts@hackreactor.com>
2015-07-04 11:23:18 -0700

    Solved all tests for hashTable!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,42 @@ HashTable.prototype.insert = function(k, v){
   // 1. Get index (address) for our key (k)
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  if (this._storage[i] === undefined) {
+    this._storage[i] = [];
+  }
+
   // 2. Once we have address, insert into our table at 'i' (address!)
-  this._storage[i] = [k,v];
+  this._storage[i].push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   // 1. Get index (address) for our key (k)
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // Go to storage[i]
+  
+  // Count number of elements at storage[i] (Tooples?)
+  for (var x=0; x<this._storage[i].length; x++) {
+    if (this._storage[i][x][0] === k) {
+      return this._storage[i][x][1];
+    }
+  }
+      // Iterate through each toople
+        // Find where [0] === k
+        // Return v
 
-  // 2. Return value associated with k.
-  return this._storage[i][1];
+
+  // Return value associated with k.
+  // return this._storage[i][1];
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  for (var x=0; x<this._storage[i].length; x++) {
+    if (this._storage[i][x][0] === k) {
+      this._storage[i][x][1] = null;
+    }
+  }
 };
 
 
@@ -29,6 +51,7 @@ HashTable.prototype.remove = function(k){
  * Complexity: What is the time complexity of the above functions?
  */
 
- var myTable = new HashTable();
- myTable.insert('Eric','Fischer');
 
+ var myTable = new HashTable();
+ myTable.insert('val1','val1');
+ myTable.insert('val2','val2');
